21402043dea3305e3a7151c22e55c93cc553079d
guga-ga <gugaga@tutamail.com>
2024-08-01 00:13:34 +0300

    Update letschatapp_page.js

--- letschatapp_page.js
+++ letschatapp_page.js
@@ -1,14 +1,4 @@
-//YOUR FIREBASE LINKS
-var firebaseConfig = {
-      apiKey: "AIzaSyAa_F_XPoUgs3fDSJLswTZhQrxv7rZj6_0",
-      authDomain: "kwitter-project-69bb1.firebaseapp.com",
-      databaseURL: "https://kwitter-project-69bb1-default-rtdb.firebaseio.com",
-      projectId: "kwitter-project-69bb1",
-      storageBucket: "kwitter-project-69bb1.appspot.com",
-      messagingSenderId: "836414661057",
-      appId: "1:836414661057:web:ba5388ac217767dca20f40"
-    };
-    // Initialize Firebase
+
     firebase.initializeApp(firebaseConfig);
 
     user_name=localStorage.getItem("user_name");
