ab8efedb779b1dd5ecd6f044602cf33538b3fee9
guga-ga <gugaga@tutamail.com>
2024-08-01 00:12:24 +0300

    Update letschatapp_room.js

--- letschatapp_room.js
+++ letschatapp_room.js
@@ -1,14 +1,4 @@
-var firebaseConfig = {
-      apiKey: "AIzaSyAa_F_XPoUgs3fDSJLswTZhQrxv7rZj6_0",
-      authDomain: "kwitter-project-69bb1.firebaseapp.com",
-      databaseURL: "https://kwitter-project-69bb1-default-rtdb.firebaseio.com",
-      projectId: "kwitter-project-69bb1",
-      storageBucket: "kwitter-project-69bb1.appspot.com",
-      messagingSenderId: "836414661057",
-      appId: "1:836414661057:web:ba5388ac217767dca20f40"
-    };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
+
 
     user_name=localStorage.getItem("user_name");
     document.getElementById("user_name").innerHTML="Welcome " +user_name+ "!";
